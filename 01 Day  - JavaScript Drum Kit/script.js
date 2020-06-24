1d0347fdf5bd09396e13dd65f6d18f775e671995
anbreaker <antunez19@gmail.com>
2020-06-24 19:20:32 +0200

    view a littel animation

--- 01 Day  - JavaScript Drum Kit/script.js
+++ 01 Day  - JavaScript Drum Kit/script.js
@@ -1,5 +1,16 @@
-console.log('Hola mundo');
-
 window.addEventListener('keydown', function (e) {
-  console.log(e);
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+  if (!audio) return; // Stop function from running all together
+
+  audio.currentTime = 0; // Rewind to the start
+  audio.play();
+  console.log(key);
+  key.classList.add('playing');
+
+  setTimeout(() => {}, 0.6);
 });
+
+// https://courses.wesbos.com/account/access/5ef37440055a68151fb7cf67/view/194130650
+
+// Minut 12
