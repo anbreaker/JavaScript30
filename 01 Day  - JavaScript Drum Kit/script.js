449fdba1b4064ad61e4a104e681f6e617ff8de2b
anbreaker <antunez19@gmail.com>
2020-06-25 12:48:37 +0200

    viw event for transform

--- 01 Day  - JavaScript Drum Kit/script.js
+++ 01 Day  - JavaScript Drum Kit/script.js
@@ -1,16 +1,23 @@
 window.addEventListener('keydown', function (e) {
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
   const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
-  if (!audio) return; // Stop function from running all together
+  if (!audio) {
+    return; // Stop function from running all together
+  }
 
   audio.currentTime = 0; // Rewind to the start
   audio.play();
-  console.log(key);
+  // console.log(key);
   key.classList.add('playing');
 
-  setTimeout(() => {}, 0.6);
+  setTimeout(() => {}, 0.07);
 });
 
-// https://courses.wesbos.com/account/access/5ef37440055a68151fb7cf67/view/194130650
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // Skip it if it's not a transform
+  console.log(e.propertyName);
+}
 
-// Minut 12
+const keys = document.querySelectorAll('.key');
+
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
