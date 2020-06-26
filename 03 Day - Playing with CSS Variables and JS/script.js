ee3863ae221a26695c8a699edf689e65f6b33f17
anbreaker <antunez19@gmail.com>
2020-06-26 19:32:55 +0200

    finish control css variable with js

--- 03 Day - Playing with CSS Variables and JS/script.js
+++ 03 Day - Playing with CSS Variables and JS/script.js
@@ -1,1 +1,12 @@
-console.log('hola');
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdated() {
+  // console.log(this.value);
+  const suffix = this.dataset.sizing || '';
+  // console.log(suffix);
+  // console.log(this.name);
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach((input) => input.addEventListener('change', handleUpdated));
+inputs.forEach((input) => input.addEventListener('mousemove', handleUpdated));
